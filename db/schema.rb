c149ecbefcdba4d9057ad089fd221f70bad41ada
David Jarillo <vjdavid621@gmail.com>
2021-01-17 12:50:35 -0800

    creating model, migration, factoy and model spec of Course

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_17_204013) do
+ActiveRecord::Schema.define(version: 2021_01_17_204650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["title"], name: "index_courses_on_title"
+  end
 
   create_table "teachers", force: :cascade do |t|
     t.string "description"
---
+++ spec/factories/courses.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :course do
+    title { "MyString" }
+    description { "MyString" }
+  end
+end
