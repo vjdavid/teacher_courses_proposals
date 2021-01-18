6fcc72238b71e1c1b52ff96801c0645fd13ed502
David Jarillo <vjdavid621@gmail.com>
2021-01-18 02:56:41 -0800

    adding nested routes with proposals to submit, and courses with votes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :proposals, only: [:index] do
+    post 'submit', on: :collection
+  end
+  resources :courses, :teachers, only: [:index, :show] do
+    resources :votes, only: [:create, :destroy]
+  end
+
 end
