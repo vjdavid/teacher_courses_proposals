f108728e0255094296c311651319fd56027223a4
David Jarillo <vjdavid621@gmail.com>
2021-01-18 07:22:32 -0800

    adding spec of register teacher

--- spec/interactors/register_teacher_spec.rb
+++ spec/interactors/register_teacher_spec.rb
@@ -1,7 +1,47 @@
-require 'spec_helper'
+require 'rails_helper'
 
 RSpec.describe RegisterTeacher, type: :interactor do
+  let(:proposal_params) do
+    {
+      'description' => 'I am a awesome teacher ever',
+      'full_name' => 'Johm Doe',
+      'email' => 'sample@gmail.com',
+      'courses' => [ { :name => 'sample', :description => 'sample'} ]
+    }
+  end
+  subject(:context) { RegisterTeacher.call(proposal_params: proposal_params) }
+
   describe '.call' do
-    pending "add some examples to (or delete) #{__FILE__}"
+    context 'when given valid paramters' do
+
+      it 'succeeds' do
+        expect(context).to be_a_success
+      end
+
+      it 'provides the user email' do
+        expect(context.teacher.email).to eq(proposal_params['email'])
+      end
+    end
+
+    context 'when given invalid parameters' do
+      let(:teacher_already_registered) { create(:teacher, email: 'sample@gmail.com') }
+      let(:proposal_params) do
+        {
+          'description' => 'I am a awesome teacher',
+          'full_name' => 'Johm Doe',
+          'email' => 'sample@gmail.com'
+        }
+      end
+
+      it 'fails' do
+        teacher_already_registered
+        expect(context).to be_a_failure
+      end
+
+      it 'provides a failure message' do
+        teacher_already_registered
+        expect(context.errors).to be_present
+      end
+    end
   end
 end
